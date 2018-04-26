0e7a17f0764a4fee34729ab11d3b35a7b3ec94db
Per Flitig <per.flitig@gmail.com>
2018-04-26 18:55:32 +0200

    Fix indentation

--- features/step-definitions/google-steps.js
+++ features/step-definitions/google-steps.js
@@ -9,21 +9,21 @@ When('jag söker på Google efter {string}', function (searchQuery, next) {
   this.driver.get('http://www.google.co.uk/webhp?complete=0');
 
   this.driver.findElement(By.name('q'))
-             .sendKeys(searchQuery);
+    .sendKeys(searchQuery);
 
   this.driver.findElement(By.name('q'))
-             .sendKeys(Key.ENTER)
-             .then(function() {
-               next();
-             });
+    .sendKeys(Key.ENTER)
+    .then(function() {
+      next();
+    });
 });
 
 Then('borde jag se sökresultat', function (next) {
   this.driver.wait(until.elementLocated(By.css('div.g')));
-
+  
   this.driver.findElements(By.css('div.g'))
-             .then(function(elements) {
-               expect(elements.length).to.not.equal(0);
-               next();
-             });
+    .then(function(elements) {
+      expect(elements.length).to.not.equal(0);
+      next();
+    });
 });
