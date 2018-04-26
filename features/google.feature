0f83b26f5cde7b12e9ea15c24684ff5766a4ef8a
Per Flitig <per.flitig@gmail.com>
2018-04-26 18:49:23 +0200

    Create example project

---
+++ features/google.feature
@@ -0,0 +1,10 @@
+# language: sv
+
+Egenskap: Searching for cucumbers
+  I egenskap av en internetanvändare
+  Så vill jag kunna söka efter information om gurkor
+  För att hitta information om gurkor
+
+  Scenario: Google sökning efter gurkor
+    När jag söker på Google efter "gurkor"
+    Så borde jag se sökresultat
---
+++ features/step-definitions/google-steps.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let { When, Then } = require('cucumber');
+let { By, until, Key } = require('selenium-webdriver');
+let { expect } = require('chai');
+
+
+When('jag söker på Google efter {string}', function (searchQuery, next) {
+  this.driver.get('http://www.google.co.uk/webhp?complete=0');
+
+  this.driver.findElement(By.name('q'))
+             .sendKeys(searchQuery);
+
+  this.driver.findElement(By.name('q'))
+             .sendKeys(Key.ENTER)
+             .then(function() {
+               next();
+             });
+});
+
+Then('borde jag se sökresultat', function (next) {
+  this.driver.wait(until.elementLocated(By.css('div.g')));
+
+  this.driver.findElements(By.css('div.g'))
+             .then(function(elements) {
+               expect(elements.length).to.not.equal(0);
+               next();
+             });
+});
